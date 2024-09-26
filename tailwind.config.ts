52d3484827eb34bf66b77bd867cfed00d1bf5779
Abd Elrahman Essam Mohamed <bodyessam97a@gmail.com>
2024-09-26 03:01:47 +0300

    first state

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        Sky: "#C3EBFA",
+        SkyLight: "#EDF9FD",
+        Purple: "#CFCEFF",
+        PurpleLight: "#F1F0FF",
+        Yellow: "#FAE27C",
+        YellowLight: "#FEFCE8",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
